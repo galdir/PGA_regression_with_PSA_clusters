4cc8bd754fcd272c34f6eb717d3eb8ee4d7ce1f1
Galdir Reges <galdir@gmail.com>
2026-04-06 15:12:18 -0300

    feat: update hyperparameters for XGBoost and Random Forest models; enhance tuning results and improve performance metrics

--- modeling.py
+++ modeling.py
@@ -224,8 +224,8 @@ def tune_xgboost(X_train: pd.DataFrame, y_train: pd.Series, groups: pd.Series,
             'n_estimators': 2000, # Fixado em um valor alto; o early_stopping decide a parada
             'learning_rate': trial.suggest_float('learning_rate',  1e-3, 0.3, log=True), # Força um aprendizado mais lento (mais árvores)
             'max_depth': trial.suggest_int('max_depth', 3, 15), # Limita a profundidade para evitar overfit nas folhas
-            'subsample': trial.suggest_float('subsample', 0.5, 1.0),
-            'colsample_bytree': trial.suggest_float('colsample_bytree', 0.5, 1.0),
+            'subsample': trial.suggest_float('subsample', 0.2, 1.0),
+            'colsample_bytree': trial.suggest_float('colsample_bytree', 0.2, 1.0),
             'min_child_weight': trial.suggest_int('min_child_weight', 0, 25),
             'gamma': trial.suggest_float('gamma', 1e-8, 1.0, log=True),
             'reg_alpha': trial.suggest_float('reg_alpha', 1e-3, 10.0, log=True),
